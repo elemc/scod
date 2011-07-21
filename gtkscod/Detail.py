2f75972ce9ffe7479e03746ec3301ec584ba5e22
Flash <Flash@localhost.localdomain>
2011-07-22 03:43:44 +0400

    began gtk gui;

---
+++ gtkscod/Detail.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+
+separator = gtk.HSeparator()
+
+nameLabel = gtk.Label()
+nameLabel.set_text('Name :')
+nameLabel.set_justify(gtk.JUSTIFY_LEFT)
+nameLabel.set_alignment(0.0, 0.0)
+selectModuleLabel = gtk.Label()
+selectModuleLabel.set_text('Select Module :')
+selectModuleLabel.set_justify(gtk.JUSTIFY_LEFT)
+selectModuleLabel.set_alignment(0.0, 0.0)
+actionDeviceLabel = gtk.Label()
+actionDeviceLabel.set_text('Action Device :')
+actionDeviceLabel.set_justify(gtk.JUSTIFY_LEFT)
+actionDeviceLabel.set_alignment(0.0, 0.0)
+neededActionLabel = gtk.Label()
+neededActionLabel.set_text('For installing this module need :')
+neededActionLabel.set_justify(gtk.JUSTIFY_LEFT)
+neededActionLabel.set_alignment(0.0, 0.0)
+installPacksLabel = gtk.Label()
+installPacksLabel.set_text('Packages to install :')
+installPacksLabel.set_justify(gtk.JUSTIFY_LEFT)
+installPacksLabel.set_alignment(0.0, 0.0)
+
+text = gtk.TextView()
+text.set_tooltip_text('Actions')
+text.set_size_request(200, 150)
+
+reset = gtk.Button('Reset')
+reset.set_alignment(0.0, 0.0)
+accept = gtk.Button('Accept')
+accept.set_alignment(1.0, 1.0)
+
+hbox = gtk.HBox(False, 1)
+hbox.pack_start(reset, False, True, 0)
+hbox.pack_end(accept, False, True, 0)
+reset.show()
+accept.show()
+
+Detail = gtk.VBox(False, 1)
+Detail.set_border_width(1)
+
+Detail.pack_start(nameLabel, False, False, 0)
+Detail.pack_start(selectModuleLabel, False, False, 0)
+Detail.pack_start(actionDeviceLabel, False, False, 0)
+Detail.pack_start(text, False, False, 0)
+Detail.pack_start(neededActionLabel, False, False, 0)
+Detail.pack_start(installPacksLabel, False, False, 0)
+Detail.pack_start(separator, False, True, 0)
+Detail.pack_start(hbox, False, True, 0)
+separator.show()
+text.show()
+actionDeviceLabel.show()
+neededActionLabel.show()
+installPacksLabel.show()
+selectModuleLabel.show()
+hbox.show()
+nameLabel.show()
---
+++ gtkscod/gtkScodClient.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+from Detail import Detail
+
+class gtkScodClient:
+	# Obligatory basic callback
+	def print_hello(self, w, data):
+		print "Hello, World!"
+
+	def get_main_menu(self, window):
+		accel_group = gtk.AccelGroup()
+
+		# This function initializes the item factory.
+		# Param 1: The type of menu - can be MenuBar, Menu,
+		#		  or OptionMenu.
+		# Param 2: The path of the menu.
+		# Param 3: A reference to an AccelGroup. The item factory sets up
+		#		  the accelerator table while generating menus.
+		item_factory = gtk.ItemFactory(gtk.MenuBar, "<main>", accel_group)
+
+		# This method generates the menu items. Pass to the item factory
+		#  the list of menu items
+		item_factory.create_items(self.menu_items)
+
+		# Attach the new accelerator group to the window.
+		window.add_accel_group(accel_group)
+
+		# need to keep a reference to item_factory to prevent its destruction
+		self.item_factory = item_factory
+		# Finally, return the actual menu bar created by the item factory.
+		return item_factory.get_widget("<main>")
+
+	def __init__(self):
+		self.menu_items = (
+			( "/_File", \
+							None,			None,				0, "<Branch>" ),
+			( "/File/E_xit", \
+							"<control>Q",	gtk.main_quit,		0, None ),
+			( "/_Devices", \
+							None,			None,				0, "<Branch>" ),
+			( "/Devices/_Disable notification", \
+							None,			None,				0, None ),
+			( "/Devices/Disable notif for _all", \
+							None,			None,				0, None ),
+			( "/_Actions", \
+							None,			None,				0, "<Branch>" ),
+			( "/Actions/on _Disable notif", \
+							None,			None,				0, None ),
+			( "/Actions/_Apply all actions", \
+							None,			None,				0, None ),
+			( "/Actions/_Cancel actions", \
+							None,			None,				0, None ),
+			( "/_Help", \
+							None,			None,				0, "<LastBranch>" ),
+			( "/Help/About", \
+							None,			None,				0, None ),
+			( "/Help/About Qt", \
+							None,			None,				0, None ),
+			)
+
+		toolbar = gtk.Toolbar()
+		#toolbar.prepend_item(text, tooltip_text, tooltip_private_text, icon, callback, user_data)
+		toolbar.append_item('Disable\nnotification', "Disable notification", '', None, None, user_data = None)
+		toolbar.append_item('Disable\nnotif for all', "Disable notif for all", '', None, None, user_data = None)
+		toolbar.append_item('Apply\nall actions', "Apply all actions", '', None, None, user_data = None)
+		toolbar.append_item('Delete\naction', "Delete action", '', None, None, user_data = None)
+		toolbar.append_item('Cancel\nactions', "Cancel actions", '', None, None, user_data = None)
+		#toolbar.append_space()
+		toolbar.prepend_space()
+		#toolbar.insert_space(position)
+		toolbar.set_orientation(gtk.ORIENTATION_HORIZONTAL)
+		toolbar.set_style(gtk.TOOLBAR_BOTH)
+		toolbar.set_border_width(1)
+
+		#separator = gtk.HSeparator()
+
+		listDev = gtk.TextView()
+		listDev.set_tooltip_text('Detected Devices')
+		listDev.set_size_request(200, 150)
+
+		scrollWindow = gtk.ScrolledWindow()
+		scrollWindow.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+		scrollWindow.add_with_viewport(Detail)
+		Detail.show()
+
+		#detail = gtk.VBox(False, 1)
+		detail = gtk.Frame()
+		detail.set_label('Modules')
+		detail.set_tooltip_text('Module details')
+		detail.set_border_width(1)
+		detail.add(scrollWindow)
+		scrollWindow.show()
+
+		hpaned = gtk.HPaned()
+		hpaned.add1(listDev)
+		hpaned.add2(detail)
+		hpaned.set_size_request(600, 300)
+		listDev.show()
+		detail.show()
+
+		text = gtk.TextView()
+		text.set_tooltip_text('Actions')
+		text.set_size_request(600, 150)
+
+		vpaned = gtk.VPaned()
+		vpaned.add1(hpaned)
+		vpaned.add2(text)
+		hpaned.show()
+		text.show()
+
+		window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+		window.connect("destroy", lambda w: gtk.main_quit())
+		window.set_title("GTK ScodClient")
+		window.set_size_request(600, 400)
+
+		main_vbox = gtk.VBox(False, 1)
+		main_vbox.set_border_width(1)
+		window.add(main_vbox)
+		main_vbox.show()
+
+		menubar = self.get_main_menu(window)
+
+		main_vbox.pack_start(menubar, False, True, 0)
+		#main_vbox.pack_start(separator, False, True, 0)
+		main_vbox.pack_start(toolbar, False, True, 0)
+		main_vbox.pack_start(vpaned, False, True, 0)
+		toolbar.show()
+		#separator.show()
+		menubar.show()
+		vpaned.show()
+		window.show()
+
+def main():
+	gtk.main()
+	return 0
+
+if __name__ == "__main__":
+	gtkScodClient()
+	main()
